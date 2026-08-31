82fd016249234a5054a7313164f65dc39ac2e8fd
torbilote <norbert.lipinski96@gmail.com>
2026-08-31 13:30:20 +0200

    updated typing

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
 import importlib
 import os
-from collections.abc import Iterator
+from collections.abc import Generator, Iterator
 from contextlib import contextmanager
 
 import pytest
@@ -17,7 +17,7 @@ _CSS_CLASS_ENV_VARS = [
 
 
 @contextmanager
-def _temporary_env(name: str, value: str | None) -> Iterator[None]:
+def _temporary_env(name: str, value: str | None) -> Generator[None, None, None]:
     """Set (or unset) an env var, restoring the original value afterwards."""
     original = os.environ.get(name)
     if value is None:
